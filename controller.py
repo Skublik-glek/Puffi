04d48aa598825b2bbb6bd72731f34729b1b986f1
arkadij <sadovnikovarkadyi2003@gmail.com>
2023-04-27 10:16:38 +0700

    continue story podval v13

--- controller.py
+++ controller.py
@@ -1,8 +1,9 @@
+import self as self
+
 from gui_elements import *
 import pygame as pg
 from sound_manager import *
 import sys
-
 
 import random
 
@@ -71,6 +72,7 @@ class GrandmaVacuumCleaner(Character):
         super().ultimate()
         return "Borsch."
 
+
 class intro():
     def __init__(self, character: Character):
         self.character = character
@@ -86,16 +88,17 @@ class intro():
     def next(self):
         if game_gui.update_text.done and not game_gui.choises.done:
             game_gui.choises.send({"играть": [1, pg.Color('black'), pg.Color('lightgreen'),
-                                                     pg.Color('white')],
+                                              pg.Color('white')],
                                    "выйти": [2, pg.Color('black'),
-                                                     pg.Color('lightgreen'),
-                                                     pg.Color('white')]})
+                                             pg.Color('lightgreen'),
+                                             pg.Color('white')]})
         if game_gui.choises.result == 1:
             game_gui.choises.result = None
             game_gui.choises.done = False
             loc_manager.loc = epilogue(self.character)
         if game_gui.choises.result == 2:
             sys.exit()
+
 
 class epilogue():
     def __init__(self, character: Character):
@@ -113,7 +116,6 @@ class epilogue():
         self.next_action = self.next
         # тест ллд
 
-
     def next(self):
         if game_gui.next_text and game_gui.update_text.done and not game_gui.choises.done:
             game_gui.next_text = False
@@ -130,18 +132,19 @@ class StartLocacion():
         game_gui.choises.result = None
         game_gui.choises.active = False
         game_gui.choises.done = False
-        game_gui.update_text.send_text("""Вы  спите у себя дома, как вдруг  вас будит  громкий звук. Вы просыпаетесь """)
+        game_gui.update_text.send_text(
+            """Вы  спите у себя дома, как вдруг  вас будит  громкий звук. Вы просыпаетесь """)
         self.next_action = self.next
-                                       
+
     def next(self):
         if game_gui.update_text.done and not game_gui.choises.done:
             game_gui.choises.send({
-                                   "Встать и покормить домашнее животное": [2, pg.Color('black'),
-                                                                            pg.Color('lightgreen'),
-                                                                            pg.Color('white')],
-                                   "Встать и спрятаться Вниз": [3, pg.Color('black'),
-                                                                     pg.Color('lightgreen'),
-                                                                     pg.Color('white')]})
+                "Встать и покормить домашнее животное": [2, pg.Color('black'),
+                                                         pg.Color('lightgreen'),
+                                                         pg.Color('white')],
+                "Встать и спрятаться Вниз": [3, pg.Color('black'),
+                                             pg.Color('lightgreen'),
+                                             pg.Color('white')]})
         if game_gui.choises.result == 1 or game_gui.choises.result == 3:
             game_gui.choises.result = None
             game_gui.update_text.send_text("""Вы обязательно должны покормить домашний тостер""")
@@ -156,9 +159,9 @@ class StartLocacion():
     def next2(self):
         if game_gui.update_text.done and not game_gui.choises.done:
             game_gui.choises.send({
-                                   "Встать и спрятаться  На нижний этаж": [2, pg.Color('black'),
-                                                                     pg.Color('lightgreen'),
-                                                                     pg.Color('white')]})
+                "Встать и спрятаться  На нижний этаж": [2, pg.Color('black'),
+                                                        pg.Color('lightgreen'),
+                                                        pg.Color('white')]})
         if game_gui.choises.result == 2:
             game_gui.choises.result = None
             game_gui.choises.done = False
@@ -190,11 +193,11 @@ class podval_loc():
 
     def next(self):
         if game_gui.update_text.done and not game_gui.choises.done:
-                game_gui.choises.send({"Выйти из Дома": [1, pg.Color('black'), pg.Color('lightgreen'),
-                                                                  pg.Color('white')],
-                                       "Остаться Дома": [2, pg.Color('black'),
-                                                                                pg.Color('lightgreen'),
-                                                                                pg.Color('white')]})
+            game_gui.choises.send({"Выйти из Дома": [1, pg.Color('black'), pg.Color('lightgreen'),
+                                                     pg.Color('white')],
+                                   "Остаться Дома": [2, pg.Color('black'),
+                                                     pg.Color('lightgreen'),
+                                                     pg.Color('white')]})
         if game_gui.choises.result == 2:
             game_gui.choises.result = None
             game_gui.choises.done = False
@@ -204,7 +207,6 @@ class podval_loc():
             game_gui.choises.result = None
             game_gui.choises.done = False
             loc_manager.loc = street_loc(self.character)
-
 
 
 class street_loc():
@@ -219,11 +221,13 @@ class street_loc():
 Чтоб спасти весь свой народ пылесосов, вы решили добраться до главного босса и отомстить ему
  """)
         self.next_action = self.next
-        
+
     def next(self):
         if game_gui.next_text and game_gui.update_text.done and not game_gui.choises.done:
             game_gui.next_text = False
             loc_manager.loc = TinaKandelaki(self.character)
+
+
 class TinaKandelaki():
     def __init__(self, character: Character):
         self.character = character
@@ -240,21 +244,22 @@ class TinaKandelaki():
 
     def next(self):
         if game_gui.update_text.done and not game_gui.choises.done:
-            game_gui.choises.send({"нырнуть в болото": [1, pg.Color('black'), pg.Color('lightgreen'),
-                                                     pg.Color('white')],
-                                   "Переплыть болото и пойти в лес": [2, pg.Color('black'),
-                                                     pg.Color('lightgreen'),
-                                                     pg.Color('white')]})
+            game_gui.choises.send({"Переплыть болото и пройти в лес": [1, pg.Color('black'), pg.Color('lightgreen'),
+                                                                       pg.Color('white')],
+                                   "нырнуть в болото": [2, pg.Color('black'),
+                                                        pg.Color('lightgreen'),
+                                                        pg.Color('white')]})
         if game_gui.choises.result == 1:
             game_gui.choises.result = None
             game_gui.choises.done = False
-            loc_manager.loc = loc_les(self.character)
+            loc_manager.loc = les_loc(self.character)
         if game_gui.choises.result == 2:
             game_gui.choises.result = None
             game_gui.choises.done = False
             loc_manager.loc = lab_loc(self.character)
 
-class loc_les():
+
+class lab_loc():
     def __init__(self, character: Character):
         self.character = character
         game_gui.background.send("data/pictures/lab_loc.jpg")
@@ -267,13 +272,13 @@ class loc_les():
 
         self.next_action = self.next
 
-
     def next(self):
         if game_gui.next_text and game_gui.update_text.done and not game_gui.choises.done:
             game_gui.next_text = False
             loc_manager.loc = space_loc(self.character)
 
-class lab_loc():
+
+class les_loc():
     def __init__(self, character: Character):
         self.character = character
         game_gui.background.send("data/pictures/loc_les.jpg")
@@ -281,15 +286,16 @@ class lab_loc():
         game_gui.choises.choises = {}
         game_gui.choises.active = False
         game_gui.choises.done = False
-        game_gui.update_text.send_text("""""")
+        game_gui.update_text.send_text("""Подойдя к лесу вы натыкаетесь на ещё одного прислужника босса и пытаетесь убежать от него """)
 
         self.next_action = self.next
-
 
     def next(self):
         if game_gui.next_text and game_gui.update_text.done and not game_gui.choises.done:
             game_gui.next_text = False
-            loc_manager.loc = space_loc(self.character)
+            loc_manager.loc = choice_loc(self.character)
+
+
 class space_loc():
     def __init__(self, character: Character):
         self.character = character
@@ -302,21 +308,19 @@ class space_loc():
 
         self.next_action = self.next
 
-
-
     def next(self):
         if game_gui.update_text.done and not game_gui.choises.done:
             game_gui.choises.send({"Улица": [1, pg.Color('black'), pg.Color('lightgreen'),
-                                                     pg.Color('white')],
+                                             pg.Color('white')],
                                    "дом": [2, pg.Color('black'),
-                                              pg.Color('lightgreen'), pg.Color('white')],
-                                           "Болото": [3, pg.Color('black'), pg.Color('lightgreen'),
-                                                       pg.Color('white')],
-                                           "Босс": [4, pg.Color('black'),
-                                                     pg.Color('lightgreen'),
-                                               pg.Color('white')]})
-            frog = Frog(500, "data/pictures/frog.png")
-            frogs.add(frog)
+                                           pg.Color('lightgreen'), pg.Color('white')],
+                                   "Болото": [3, pg.Color('black'), pg.Color('lightgreen'),
+                                              pg.Color('white')],
+                                   "Босс": [4, pg.Color('black'),
+                                            pg.Color('lightgreen'),
+                                            pg.Color('white')]})
+            # frog = Frog(500, "data/pictures/frog.png")
+            # frogs.add(frog)
 
         if game_gui.choises.result == 1:
             game_gui.choises.result = None
@@ -334,7 +338,7 @@ class space_loc():
         if game_gui.choises.result == 4:
             game_gui.choises.result = None
             game_gui.choises.done = False
-            loc_manager.loc = factory_loc(self.character)
+            loc_manager.loc = choice_loc(self.character)
 
 
 class factory_loc():
@@ -350,12 +354,10 @@ class factory_loc():
 
         self.next_action = self.next
 
-
-
     def next(self):
         if game_gui.next_text and game_gui.update_text.done and not game_gui.choises.done:
             game_gui.next_text = False
-            loc_manager.loc = factory_loc(self.character)
+            loc_manager.loc = good_loc(self.character)
 
 
 class BossFight():
@@ -368,10 +370,66 @@ class BossFight():
         self.boss_activity = "Ничего не делает"
 
 
-        # принт сюжета
+class choice_loc():
+    def __init__(self, character: Character):
+        self.character = character
+        game_gui.background.send("data/pictures/choice_loc.png")
+        sound_manager = Music("data/music/psiho.mp3")
+        game_gui.choises.choises = {}
+        game_gui.choises.active = False
+        game_gui.choises.done = False
+        game_gui.update_text.send_text("""Сделай свой последний выбор""")
+
+        self.next_action = self.next
+
+    def next(self):
+        if game_gui.update_text.done and not game_gui.choises.done:
+            game_gui.choises.send({"Тёмные стороны": [1, pg.Color('black'), pg.Color('lightgreen'),
+                                                      pg.Color('white')],
+                                   "Сторона света": [2, pg.Color('black'),
+                                                     pg.Color('lightgreen'),
+                                                     pg.Color('white')]})
+        if game_gui.choises.result == 1:
+            game_gui.choises.result = None
+            game_gui.choises.done = False
+            loc_manager.loc = bad_loc(self.character)
+        if game_gui.choises.result == 2:
+            game_gui.choises.result = None
+            game_gui.choises.done = False
+            loc_manager.loc = factory_loc(self.character)
 
 
+class bad_loc():
+    def __init__(self, character: Character):
+        self.character = character
+        game_gui.background.send("data/pictures/bad_loc.png")
+        sound_manager = Music("data/music/psiho.mp3")
+        game_gui.choises.choises = {}
+        game_gui.choises.active = False
+        game_gui.choises.done = False
+        game_gui.update_text.send_text("""Хендриксу удается вас Переубедить и заманить к себе вы становитесь приверженцев 
+роботов пылесосов а ваши деревни ненавидит 
+аас и вы больше никогда туда не пройдёте""")
 
+class good_loc():
+    def __init__(self, character: Character):
+        self.character = character
+        game_gui.background.send("data/pictures/good_loc.jpg")
+        sound_manager = Music("data/music/psiho.mp3")
+        game_gui.choises.choises = {}
+        game_gui.choises.active = False
+        game_gui.choises.done = False
+        game_gui.update_text.send_text("""Вы спасли свою деревню вместе с спасёнными из 
+    плена лягушками и мудрой жабой ай мэс празднуете победу на гл проще площади 
+    вы воздвигли себе нерукотворный памятник и теперь вас уважает и учёный и ученик а во всей длине 
+    пылесосов ходит до вас великая слава""")
+
+        self.next_action = self.next
+
+    def next(self):
+        if game_gui.next_text and game_gui.update_text.done and not game_gui.choises.done:
+            game_gui.next_text = False
+            loc_manager.loc = good_loc(self.character)
     def step(self):
         print(f"""Ваш ход!
 
@@ -426,6 +484,7 @@ class BossFight():
 
     def bossStep(self):
         pass
+
 
 
 background = Background('data/pictures/background.jpg')
